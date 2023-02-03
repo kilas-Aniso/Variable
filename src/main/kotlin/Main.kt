07ec8173bf2a6b13edf568f2cb4c438a2a9aee83
Your Name <kilasaniso@gmail.com>
2023-02-03 07:31:24 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,15 @@ fun main(){
     var fullName = "Aniso Mohamed Kilas"
     println(fullName)
 
-    var age = (20)
+    var age = (19)
     println(age)
 
     var phoneNumber = "0724638771"
     println(phoneNumber)
+
+    var weightinKg = (57.5)
+    println(weightinKg)
+
 
     var kenyanCItizen: Boolean = false
     println(kenyanCItizen)
